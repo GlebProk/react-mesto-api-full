13f33e4e3eac5a6140ef540d1c896855d7f7debf
Gleb Prokofyev <prokofyevg@yandex.ru>
2022-10-14 17:26:38 +0300

    ПР15 front and back

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,7 +11,7 @@ import EditProfilePopup from './EditProfilePopup';
 import EditAvatarPopup from './EditAvatarPopup';
 import AddPlacePopup from './AddPlacePopup';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
-import { Route, Redirect, Switch, useHistory } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch, Link, useHistory } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
 import InfoTooltip from './InfoTooltip';
